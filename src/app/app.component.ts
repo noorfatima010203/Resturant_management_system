d459747e62ac5d34c7c39b3e7281255d34353f4d
NoorFatima <noorfatima010203@gmail.com>
2026-07-21 13:04:09 +0500

    WITHOUT LOCAL STORAGE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FoodComponent } from './components/form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FoodComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rest_app_sys';
+  title = 'your-project-name';
 }
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,180 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+export interface FoodEntry {
+  FoodName: string;
+  Price: number;
+  Description: string;
+  Quantity: number;
+}
+@Component({
+  selector: 'app-food',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FoodComponent {
+  showForm = false;
+  paymentMethod = 'Card';   
+  pageSize = 10;
+  cardTax = 2.5;
+  currentPage = 1;
+  private fb = inject(FormBuilder);
+  foodForm: FormGroup = this.fb.group({
+    FoodName: ['', Validators.required],
+    Description: ['', Validators.required],
+    Price: [0, [Validators.required, Validators.min(1)]],
+    Quantity: [1, [Validators.required, Validators.min(1)]],
+  });
+  foodList: FoodEntry[] = [];
+  editingIndex: number | null = null;
+  onSubmit() {
+  if (!this.foodForm.valid) {
+    this.setlocalStorage
+    return;
+  }
+  if (this.editingIndex !== null) {
+    this.foodList[this.editingIndex] = this.foodForm.value;
+    this.editingIndex = null;
+  } else {
+    this.foodList.push(this.foodForm.value);
+  }
+  this.foodForm.reset({
+  FoodName: '',
+  Description: '',
+  Price: 0,
+  Quantity: 1
+});
+  this.showForm = false;
+}
+  paginatedFoodList(): FoodEntry[] {
+  const start = (this.currentPage - 1) * this.pageSize;
+  const end = start + this.pageSize;
+  return this.foodList.slice(start, end);
+}
+getRowTotal(food: FoodEntry): number {
+  return food.Price * food.Quantity;
+}
+//total pages
+  totalPages(): number {
+    return Math.ceil(
+      this.foodList.length / this.pageSize
+    ) ;
+  }
+  pages(): number[] {
+    return Array.from(
+      {
+        length: this.totalPages()
+      },
+      (j, i) => i + 1
+    );
+  }
+//chnage page
+  changePage(page: number): void {
+    this.currentPage = page;
+  }
+  //next pg
+  nextPage(): void {
+    if (
+      this.currentPage < this.totalPages()
+    ) {
+      this.currentPage++;
+    }
+  }
+//prev pg
+  previousPage(): void {
+    if (
+      this.currentPage > 1
+    ) {
+      this.currentPage--;
+    }
+  }
+//change
+  changePageSize(): void {
+    this.currentPage = 1;
+  }
+  editFood(food: FoodEntry): void {
+  this.showForm = true;
+  this.foodForm.patchValue(food);
+  this.editingIndex = this.foodList.indexOf(food);
+}
+//delete food
+deleteFood(food: FoodEntry): void {
+  const index = this.foodList.indexOf(food);
+  if (index > -1) {
+    this.foodList.splice(index, 1);
+  }
+}
+//total
+getGrandTotal(): number {
+   return this.foodList.reduce(
+      (sum, food) => sum + this.getRowTotal(food),
+      0
+   );
+}
+increaseQuantity(food: FoodEntry): void{
+  food.Quantity++;
+}
+decreaseQuantity(food: FoodEntry): void{
+  if(food.Quantity > 1){
+    food.Quantity--;
+  }
+}
+getTax(): number {
+  if (this.paymentMethod === 'Card') {
+    return this.getGrandTotal() * this.cardTax / 100;
+  } else {
+    return this.getGrandTotal() * 0.16;
+  }
+}
+// getTax():number{
+//   if(this.paymentMethod === 'Card')
+//     return this.getGrandTotal()*0.08
+//   else
+//     return this.getGrandTotal()*0.16
+// }
+
+getFinalTotal():number{
+return this.getGrandTotal()+ this.getTax();}
+
+downloadPDF(): void {
+const doc = new jsPDF();
+doc.text("Restaurant Menu", 14, 15);
+autoTable(doc, {
+  head: [["Food Name", "Description", "Price", "Quantity", "Total"]],
+  body: this.foodList.map(food => [
+    food.FoodName,
+    food.Description,
+   // "Rs. " + food.Price,
+    food.Price,
+    food.Quantity.toString(),
+    this.getRowTotal(food)
+  ]),
+  startY: 25
+});
+doc.save("Restaurant_Menu.pdf");
+}
+
+//storages
+setlocalStorage(): void {
+  console.log("Saving to local storage...");
+  localStorage.setItem("foodName", this.foodForm.value.FoodName);
+  localStorage.setItem("paymentMethod", this.paymentMethod);
+  localStorage.setItem("Tax", this.getTax().toString());
+  localStorage.setItem("grandTotal", this.getGrandTotal().toString());
+  localStorage.setItem("finalTotal", this.getFinalTotal().toString());
+}
+getlocalStorage(): void {
+  console.log("Gettingfrom local storage...");
+  const foodName = localStorage.getItem("foodName");
+  const paymentMethod = localStorage.getItem("paymentMethod");
+  const tax = localStorage.getItem("Tax");
+  const grandTotal = localStorage.getItem("grandTotal");
+  const finalTotal = localStorage.getItem("finalTotal");
+  console.log(foodName, paymentMethod, tax, grandTotal, finalTotal);
+}
+}
+
